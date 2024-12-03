9841571f562d045574d828216a4faa0d0109be4b
Bart Wolleswinkel <b.wolleswinkel@tudelft.nl>
2024-12-03 15:39:32 +0100

    Removed functions directory, updated version

--- dummypypi2/__init__.py
+++ dummypypi2/__init__.py
@@ -1,3 +1,3 @@
-from dummypypi2.functions.plots import plots
-from dummypypi2.functions.sims import sim
-from dummypypi2.functions.utils import utils
+from dummypypi2.plots import plots
+from dummypypi2.sims import sim
+from dummypypi2.utils import utils
