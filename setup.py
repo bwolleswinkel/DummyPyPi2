f1a1c3a4e96cb63212e431b3b0b476e223831fc7
Bart Wolleswinkel <b.wolleswinkel@tudelft.nl>
2026-01-02 17:34:10 +0100

    RELEASE: v0.1.13

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.1.12'
-DESCRIPTION = 'To be deleted later'
-LONG_DESCRIPTION = 'Dummy package to test direct publishing from GitHub to PyPi'
+VERSION = '0.1.13'
+DESCRIPTION = "To be deleted later"
+LONG_DESCRIPTION = "Dummy package to test direct publishing from GitHub to PyPi"
 
 # Setting up
 setup(
