79dbb136da3b5a0a144f880ff502bf1326adf362
Bart Wolleswinkel <b.wolleswinkel@tudelft.nl>
2026-01-03 16:34:16 +0100

    Copied some docs from Numpy, to make it more extensive. Also added a version number, although.. not sure if it'll work? Because _version.py is not tracked? Copilot says it does, because the file is built automatically

--- src/dummypypi2/__init__.py
+++ src/dummypypi2/__init__.py
@@ -1,8 +1,43 @@
-"""This is the main package module for DummyPyPi2"""
+"""
+DummyPyPi2
+==========
+
+Provides
+  1. Some random functionalities for demonstration purposes
+  2. A submodule `control` as a demonstration of package structure
+  3. Global configuration management via the `_config` submodule (accessible through non-private functions in the main module)
+
+How to use the documentation
+----------------------------
+Documentation is available in two forms: docstrings provided with the code (should be everywhere!), and a 'ReadTheDocs' hosted version with more elaborate explanations and examples on the DummyPyPi2 `homepage <https://dummypypi2.readthedocs.io/en/latest/>`_.
+
+The docstring examples assume that `dummypypi2` has been imported as ``dp``::
+>>> import dummypypi2 as dp
+
+Code snippets are indicated by three greater-than signs:
+>>> x = 42
+>>> x = x + 1
+
+Use the built-in ``help`` function to view a function's docstring:
+>>> help(dp.get_signed_angle)
+... # doctest: +SKIP
+
+Available sub-packages
+----------------------
+control
+    Does nothing at the moment
+
+Utilities
+---------
+__version__
+    DummyPyPi2 version string
+
+"""
 
 from .utils import *
 
 from . import control
 
+from ._version import __version__
 from ._config.algo import algo_options, set_algo_options
 from ._config.plot import set_display_options
